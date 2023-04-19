7b2f151468826cd63c3a9522ecee3a4292e9f8f6
HenrikS-A <hstromandersen@gmail.com>
2023-04-19 17:55:01 +0200

    riktig pris fikset

--- riktigPris.py
+++ riktigPris.py
@@ -1,11 +1,21 @@
 
 def riktig_pris(pris):
     
-    # Sjekker om komma er på riktig plass
-    if pris[-3] != ".": 
-        if "." not in pris: # Sjekker om det finnes komma
-            return pris + ".00"
-        elif pris[-2] == ".": # Sjekker om det bare er én desimal.
-            return pris + "0"
+    # Sjekker om dette fungerer først, men kan få indexerror hvis prisen er liten
+    try:
+        # Sjekker om komma er på riktig plass
+        if pris[-3] != ".": 
+            if "." not in pris: # Sjekker om det finnes komma
+                return pris + ".00"
+            elif pris[-2] == ".": # Sjekker om det bare er én desimal.
+                return pris + "0"
         
-    return pris
+        return pris
+    
+    except IndexError:
+            if "." not in pris: # Sjekker om det finnes komma
+                return pris + ".00"
+            elif pris[-2] == ".": # Sjekker om det bare er én desimal.
+                return pris + "0"
+
+
